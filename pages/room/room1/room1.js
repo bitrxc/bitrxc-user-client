3191e3733270ac90a4798ef576b64c75a010e7ed
jingkaimori <wuyiheng6@163.com>
2021-03-12 12:21:03 +0800

    [refactor] Adjust interaction logic
    调整交互逻辑

--- pages/room/room1/room1.js
+++ pages/room/room1/room1.js
@@ -47,121 +47,106 @@ Page({
     ],
   },
 
-  formSubmit: function (e) { //提交表单中用户填写的内容
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  /**
+   * 检查用户填写的内容
+   * 提交表单中用户填写的内容
+   * 更新视图
+   * @param {WechatMiniprogram.FormSubmit} e 
+   */
+  formSubmit:async function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    /*暂时不检查人数和姓名
     if (e.detail.value.name.length == 0) {
 
       wx.showToast({
-
         title: '姓名不能为空!',
         icon:"none",
-
-
         duration: 1500
-
       })
 
-      setTimeout(function () {
-
-        wx.hideToast()
-
-      }, 2000)
+      setTimeout(
+        () => { wx.hideToast() }, 
+        2000
+      )
 
     } else if (e.detail.value.number.length == 0) {
 
       wx.showToast({
-
         title: '人数不能为空!',
         icon:"none",
-
-
         duration: 1500
-
       })
 
-      setTimeout(function () {
+      setTimeout(
+        () => { wx.hideToast() }, 
+        2000
+      )
 
-        wx.hideToast()
-
-      }, 2000)
-
-    } else if (e.detail.value.usefor.length == 0) {
+    } else */if (e.detail.value.usefor.length == 0) {
 
       wx.showToast({
-
         title: '用途不能为空!',
         icon:"none",
-
         duration: 1500
-
       })
 
-      setTimeout(function () {
-
-        wx.hideToast()
-
-      }, 2000)
+      setTimeout(
+        () => { wx.hideToast() }, 
+        2000
+      )
 
     } else {
 
-
-     /* wx.request({
-
-        url: '',
-
-        header: {
-
-          "Content-Type": "application/x-www-form-urlencoded"
-
-        },
-
-        method: "POST",
-
-        data: { name: e.detail.value.name, 
-                number: e.detail.value.number, 
-                usefor: e.detail.value.usefor
-               },
-
-        success: function (res) {
-          console.log(res.data);
-          if (res.data.status == 0) {
-
-            wx.showToast({
-
-              title: '提交失败！',
-
-              icon: 'loading',
-
-              duration: 1500
-
-            })
-
-          } else {
-
-            wx.showToast({
-
-              title: '提交成功！',//这里打印出登录成功
-
-              icon: 'success',
-
-              duration: 1000，
-
-               wx.navigateTo({ //跳转到预约进度页面
-      url: '../../my/process/process',
-     })
-
-            }),
-
-
-          }
-
+      let apInfo = this.data.cardView;
+      let execDate = new Date(apInfo.execDate);
+      console.log(execDate);
+      let res = await request({
+        url : app.globalData.server + "/appointment/appoint",
+        header : app.globalData.APIHeader ,
+        method : "POST",
+        data : {
+          roomId : this.data.roomId,
+          launcher : app.globalData.userInfo.username,
+          launchTime : apInfo.yysd,
+          execDate : execDate.toISOString(),
+          launchDate : Date.now(),
+          userNote : e.detail.value.usefor,
         }
+      })
+      
+      console.log(res.data);
+      if (res.data.code != 200) {
 
-      })*/
+        wx.showToast({
+          title: '提交失败！',
+          icon: 'loading',
+          duration: 1500,
+        })
 
+      } else {
+        //刷新预约时段所在列
+        let list = await this.fetchColumn(execDate);
+        let dayNow = (execDate.getDay()+6) % 7+1;
+        list = list.concat(
+          this.data.wlist.filter((v)=> v.xqj != dayNow)
+        );
+        this.setData({
+          wlist:list,
+        })
+
+        await wx.showToast({
+          title: '提交成功！',//这里打印出登录成功
+          icon: 'success',
+          duration: 1000,
+        })
+        //不跳转到预约进度页面，多次预约
+        // await wx.navigateTo({ 
+        //   url: '../../my/process/process',
+        // })
+        //不播放动画，关闭
+        // this.util("close");
+      }
     }
-
-  
   },
   //初始化页面标题
   //初始化预约时间列表
@@ -321,35 +306,7 @@ Page({
   hideModal() { //点击弹框外空白处收起弹框(取消按钮相同)
     this.util("close");
   },
-  //
-  onOK:async function () {  //确定按钮
 
-    let apInfo = this.data.cardView;
-    let execDate = new Date(apInfo.execDate);
-    console.log(execDate);
-    await request({
-      url : app.globalData.server + "/appointment/appoint",
-      header : app.globalData.APIHeader ,
-      method : "POST",
-      data : {
-        roomId : this.data.roomId,
-        launcher : app.globalData.userInfo.username,
-        launchTime : apInfo.yysd,
-        execDate : execDate.toISOString(),
-        launchDate : Date.now(),
-      }
-    })
-    //刷新预约时段所在列
-    let list = await this.fetchColumn(execDate);
-    let dayNow = (execDate.getDay()+6) % 7+1;
-    list = list.concat(
-      this.data.wlist.filter((v)=> v.xqj != dayNow)
-    );
-    this.setData({
-      wlist:list,
-    })
-    this.util("close");
-  },
   util: function (currentStatu) {
     var animation = wx.createAnimation({
       duration: 100, //动画时长 
