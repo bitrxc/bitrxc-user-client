3cf2407043fafada480cc274a357944586073c11
jingkaimori <wuyiheng6@163.com>
2021-04-10 12:39:24 +0800

    [feat] userinfo edit feedback
    添加用户信息修改反馈

--- libs/data.d.js
+++ libs/data.d.js
@@ -2,6 +2,9 @@
 /**
  * @module
  */
+
+import { APIError } from "./errors.d";
+
 /** 
  * @typedef
  * @property {number} id 
@@ -26,11 +29,30 @@ export const Room = class{};
  */
 export const User = class{};
 /**
- * @typedef 
  * @template payload
+ * @typedef APIResult
  * @property {payload} data
  * @property {number} code
  * @property {string} message
  * @property {boolean} success
  */
-export const APIResult = class{};
+export const APIResult = class{
+  /**
+  * 检查后端接口的返回值
+  * @template payload
+  * @param {APIResult<payload>} data 
+  * @throws {APIError}
+  * @returns {payload}
+  */
+  static checkAPIResult(data){
+    if(data.success){
+      return data.data;
+    }else{
+      if(data.code<200||data.code>300){
+        throw new APIError(data.code,data.message);
+      }else{
+        return data.data;
+      }
+    }
+  }
+};
--- pages/my/personalSet/personalSet.js
+++ pages/my/personalSet/personalSet.js
@@ -1,24 +1,41 @@
 // @ts-check pages/my/personalSet/personalSet.js
 // TODO: 通过微信平台获取电话号码
 // TODO: 向用户请求权限，并维护全局用户信息
-import { request } from "../../../libs/request.js";
-import { User } from "../../../libs/data.d.js";
+import { delay, request } from "../../../libs/request.js";
+import { APIResult, User } from "../../../libs/data.d.js";
 import { compareVersion } from "../../../libs/compareVersion.js";
+import { APIError } from "../../../libs/errors.d.js";
 const app = getApp();
 Component({
   /**
    * 页面的初始数据
    */
   data: {
-    editable:true,
+    editable:false,
     /** @type {User} */
-    user:null,
+    user:app.globalData.userInfo,
+    status:{
+      phone:"未修改",
+      name:"",
+      organization:"",
+    }
   },
   methods:{
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: async function (options) {
+    },
+    enableEdit(e){
+      this.setData({
+        editable:true,
+      })
+    },
+    disableEdit(e){
+      this.setData({
+        editable:false,
+      })
+      this.saveUserProfile({});
     },
     /**
      * 获取明文编码的用户信息，受 {@link wx.getUserInfo} 接口变化的影响，只对旧版本小程序有效
@@ -28,7 +45,7 @@ Component({
       if(compareVersion(app.systemInfo.SDKVersion,"2.10.4") == -1){
         let rawInfo = e.detail.userInfo;
         console.log(e);
-        await this.setUserProfile({
+        await this.saveUserProfile({
           name : rawInfo.nickName,
         });
       }else{
@@ -47,7 +64,7 @@ Component({
           let {userInfo} = await wx.getUserProfile({
             desc: '预约房间需要提供真实姓名',
           });
-          await this.setUserProfile({
+          await this.saveUserProfile({
             name : userInfo.nickName,
           });
 
@@ -67,7 +84,7 @@ Component({
      */
     replaceUserName:async function (e) {
       let rawName = e.detail.value;
-      await this.setUserProfile({
+      await this.saveUserProfile({
         name : rawName,
       });
     },
@@ -77,9 +94,57 @@ Component({
      */
     replacePhone:async function (e) {
       let rawPhone = e.detail.value;
-      await this.setUserProfile({
-        phone : String(rawPhone),
+      const phonePattern = RegExp("^1(3\\d{2}|4[14-9]\\d|5([0-35689]\\d|7[1-79])|66\\d|7[2-35-8]\\d|8\\d{2}|9[13589]\\d)\\d{7}$");
+      // 电话号码校验
+      if(phonePattern.test(rawPhone)){
+        try{
+          await this.saveUserProfile({
+            phone : String(rawPhone),
+          });
+        }catch(e){
+          this.setData({
+            status:{
+              ...this.data.status,
+              phone:"未成功修改电话号码",
+            },
+            user:{
+              ...this.data.user,
+            }
+          })
+        }
+        this.setData({
+          status:{
+            ...this.data.status,
+            phone:"修改成功",
+          }
+        })
+      }else{
+        this.setData({
+          status:{
+            ...this.data.status,
+            phone:"电话号码格式不正确",
+          },
+          user:{
+            ...this.data.user,
+          }
+        })
+      }
+      await delay(2000);
+      this.setData({
+        status:{
+          ...this.data.status,
+          phone:"未修改",
+        }
       });
+    },
+    activatePhone(){
+      this.setData({
+        status:{
+          ...this.data.status,
+          phone:"修改中",
+        }
+      })
+
     },
     /**
      * 
@@ -87,27 +152,39 @@ Component({
      */
     replaceOrg:async function (e) {
       let rawOrg = e.detail.value;
-      await this.setUserProfile({
+      await this.saveUserProfile({
         organization : rawOrg,
       });
     },
-    /** @param {Partial< User | WechatMiniprogram.UserInfo>} userInfo*/
-    async setUserProfile(userInfo){
-      //TODO: 电话号码校验
+    /** 
+     * @param {Partial< User | WechatMiniprogram.UserInfo>} userInfo
+     * @returns {Promise<void>}
+     */
+    async saveUserProfile(userInfo){
       let newInfo = {
         ...this.data.user,
         ...userInfo,
       }
-      this.setData({
-        user: newInfo ,
-      })
-      let res = await request({
-        url:app.globalData.server + "/user",
-        header:app.globalData.APIHeader,
-        method:"POST",
-        data: newInfo,
-      })
-      app.globalData.userInfo = newInfo;
+      try{
+        let res = await request({
+          url:app.globalData.server + "/user",
+          header:app.globalData.APIHeader,
+          method:"POST",
+          data: newInfo,
+        })
+        APIResult.checkAPIResult(res.data);
+        this.setData({
+          user: newInfo ,
+        })
+        app.globalData.userInfo = newInfo;
+      }catch(e){
+        await wx.showToast({
+          title: '用户信息保存失败',
+        });
+        await delay(2000);
+        await wx.hideToast()
+        throw e
+      }
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
