d47f5d9d07e5999d36321a2e895d43dac1d635c7
Hxxsan <2224163815@qq.com>
2021-04-26 00:04:41 +0800

    Change show way of room list.
    改变房间页面展示列表

---
+++ components/block-view/block-view.js
@@ -0,0 +1,43 @@
+// @ts-check components/block-view/block-view.js
+Component({
+  /**
+   * 组件选项
+   */
+  options:{
+    styleIsolation:"apply-shared",
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    "list":Array,
+    "targetPage":String,
+    "targetID":String,
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  },
+  observers:{
+    'list':function (newprop) {
+      let res=[];
+      if(newprop&&newprop[Symbol.iterator]&&newprop.length){
+        res = Array.from(newprop);
+        this.setData({
+          isEmpty:false,
+        })
+      }else{
+        this.setData({
+          isEmpty:true,
+        }) 
+      }
+      return res;
+    }
+  },
+})
